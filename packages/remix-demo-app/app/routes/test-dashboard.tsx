d624e4205df35638a8c6539420924616335abb01
paterson1720 <lesage1720@gmail.com>
2024-05-09 04:58:36 -0400

    add logout button

--- packages/remix-demo-app/app/routes/test-dashboard.tsx
+++ packages/remix-demo-app/app/routes/test-dashboard.tsx
@@ -1,6 +1,6 @@
 import type { ActionFunctionArgs, MetaFunction } from "@remix-run/node";
 import auth from "../auth.server";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -24,6 +24,12 @@ export default function Dashboard() {
       <div className="flex flex-col items-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <Link
+              to="/auth/logout"
+              className="bg-red-600 text-white block w-full rounded-md p-3"
+            >
+              Logout
+            </Link>
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Welcome to Your Dashboard
             </h1>
