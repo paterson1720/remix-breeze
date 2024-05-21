8c7f424416a9b6d7b6ec2b2ed614c11588e34183
paterson1720 <lesage1720@gmail.com>
2024-05-21 12:07:48 -0400

    setup db upon app creation

--- packages/create-remix-breeze/lib/index.js
+++ packages/create-remix-breeze/lib/index.js
@@ -13,12 +13,15 @@ program
     const projectPath = path.join(process.cwd(), projectName);
     const repoUrl = "https://github.com/paterson1720/new-remix-breeze-app";
 
-    console.log(`Creating a new project in ${projectPath}.`);
+    console.info(`Creating a new project in ${projectPath}.`);
     gitClone(repoUrl, projectPath, null, function () {
-      console.log("Installing dependencies...");
+      console.info("Installing dependencies...");
       execSync("npm install", { cwd: projectPath, stdio: "inherit" });
+      console.info("Setting up the database...");
       execSync("cp .env.example .env", { cwd: projectPath, stdio: "inherit" });
-      console.log("✅ Project is ready!");
+      execSync("npx prisma db push", { cwd: projectPath, stdio: "inherit" });
+      execSync("npx prisma generate", { cwd: projectPath, stdio: "inherit" });
+      console.info("✅ Project is ready!");
     });
   })
   .parse(process.argv);
